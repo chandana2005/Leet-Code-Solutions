91ff86bec694833ff2c2572fd24aeaf1c0c19e37
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-09-20 13:15:01 +0530

    Create 198. House Robber.py

---
+++ 198. House Robber.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        prev1,prev2=0,0
+
+        for num in nums:
+            temp=max(prev1,prev2+num)
+            prev2=prev1
+            prev1=temp
+        return prev1
