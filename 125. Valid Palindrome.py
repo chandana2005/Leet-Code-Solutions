7e9324714ef64e84c25ac39ad1022862051c0411
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-09-20 13:37:37 +0530

    Create 125. Valid Palindrome.py

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr=""
+        for ch in s:
+            if ch.isalnum():
+                newstr+=ch.lower()
+        return newstr==newstr[::-1]
