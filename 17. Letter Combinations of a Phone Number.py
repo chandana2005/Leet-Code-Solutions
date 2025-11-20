83169ddbbde2b402f67f246bef59c96b0fc54b07
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-11-20 20:29:56 +0530

    Add solution for letter combinations of phone number
    Implement letter combinations for phone digits.

---
+++ 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,29 @@
+'''
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = "2"
+Output: ["a","b","c"]
+'''
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        phone={"2":"abc", "3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        res=[]
+        def backtrack(i, path):
+            if i == len(digits):
+                res.append("".join(path))
+                return
+            letters=phone[digits[i]]
+            for ch in letters:
+                path.append(ch)
+                backtrack(i+1, path)
+                path.pop()
+        backtrack(0,[])
+        return res
