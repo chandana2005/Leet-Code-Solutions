4e21feb3bf2d4b05477f29b75a61872a64a44d6f
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-11-26 13:37:17 +0530

    Add function documentation for fractionToDecimal
    Add docstring explaining the function and examples.

--- 166. Fraction to Recurring Decimal.py
+++ 166. Fraction to Recurring Decimal.py
@@ -1,3 +1,18 @@
+'''
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+If the fractional part is repeating, enclose the repeating part in parentheses
+If multiple answers are possible, return any of them.
+It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+Note that if the fraction can be represented as a finite length string, you must return it.
+
+Example 1:
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+
+Example 2:
+Input: numerator = 2, denominator = 1
+Output: "2"
+'''
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         if numerator ==0:
