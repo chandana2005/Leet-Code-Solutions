768ca160c07997d17dd1a84647732e2c53ed1397
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-12-01 14:18:38 +0530

    Add docstring for reverseBits function
    Added a docstring to explain the reverseBits function and provided an example of input and output.

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -1,3 +1,15 @@
+'''
+Reverse bits of a given 32 bits signed integer.
+
+Example 1:
+Input: n = 43261596
+Output: 964176192
+Explanation:
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+
+'''
 class Solution:
     def reverseBits(self, n: int) -> int:
         bit=format(n, '032b')
