ef37e783273ff1af578fee0118a6baecefc4c6f8
Chandana PL <125894735+chandana2005@users.noreply.github.com>
2025-12-04 14:18:49 +0530

    Create 155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,60 @@
+'''
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Implement the MinStack class:
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1:
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+'''
+
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        temp=[]
+        mini = self.stack[-1]
+        while len(self.stack):
+            mini=min(self.stack[-1], mini)
+            temp.append(self.stack.pop())
+        while len(temp):
+            self.stack.append(temp.pop())
+        return mini
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
